7c5ea4b8ce253f845ed3f67981552bbb0b4ced59
Pratham Goyal <42109004+TheRealTechWiz@users.noreply.github.com>
2022-01-06 20:47:53 +0530

    Update nyaascraper.py

--- nyaascraper.py
+++ nyaascraper.py
@@ -8,19 +8,22 @@ import sys
 import os
 import requests
 import logging
-#if you need additional info for the settings, look at the readme
-getDefaultRows: bool = False
-getDangerRows: bool = False
-TUImode: bool = True
-loggingLevel: int = logging.INFO
-baseUrl: str = 'https://nyaa.si/?s=seeders&o=desc'
-webtorrentArgs: str = "--keep-seeding --mpv"
-maxPageNum: int = 5
-dmenuArgs = {"font": "Ubuntu-15"}
-proxies = None
+getDefaultRows: bool = True #get default (white) rows on nyaa
+getDangerRows: bool = True #get danger (red) rows on nyaa
+TUImode: bool = False #use a tui instead of dmenu
+useTor: bool = True #use tor network if getting ConnectionResetError
+loggingLevel: int = logging.INFO #print debug
+baseUrl: str = 'https://nyaa.si/?s=seeders&o=desc' #base url (by default searches by most seeders)
+webtorrentArgs: str = "--keep-seeding --mpv" #args (by default starts mpv)
+maxPageNum: int = 5 #max page to get on nyaa (by default 5), if your number is too big you may encounter some delay
+dmenuArgs = {"font": "Ubuntu-15"} #additional args for dmenu
 
 if not TUImode:
     import dmenu
+
+if useTor:
+    import time
+
 
 logging.basicConfig()
 logging.getLogger().setLevel(loggingLevel)
@@ -33,15 +36,30 @@ def getRows(soup : BeautifulSoup, getDefault = getDefaultRows, getDanger = getDa
         rows += soup.find_all('tr', class_='danger')
     return rows
 
+def get_tor_session():
+    session = requests.session()
+    # Tor uses the 9050 port as the default socks port
+    session.proxies = {'http':  'socks5://127.0.0.1:9050',
+                       'https': 'socks5://127.0.0.1:9050'}
+    return session
+
 def getTorrents(url: str) -> dict:
     torrents = []
+    count = 0
     for pageNum in range(1, maxPageNum): 
         pageUrl = f"{url}&p={str(pageNum)}"
         logging.info(f"Getting page {str(pageNum)} with url {pageUrl}")
-        try: #from TheRealTechWiz, but in a slightly less dirty way
-            pageHtml = requests.get(pageUrl, proxies=proxies)
-        except: 
+
+        if useTor:    
+            s = get_tor_session()
+        try:
+            if useTor:
+                pageHtml = s.get(pageUrl)
+            else:
+                pageHtml = requests.get(pageUrl)
+        except:
             continue
+
         soup = BeautifulSoup(pageHtml.text, 'html.parser')
         logging.info(f"Got page {str(pageNum)} !")
 
@@ -51,8 +69,9 @@ def getTorrents(url: str) -> dict:
         for row in rows:
             td = row.find_all('td', class_='text-center')
             links = td[0].find_all('a')
+            size = td[1]
 
-            size = next((x for x in td if "GiB" in x.text or "$MiB" in x.text), None)
+            # size = next((x for x in td if "GiB" in x.text or "$MiB" in x.text), None)
             try:
                 size = "[" + size.get_text() + "] "
             except:
@@ -64,7 +83,8 @@ def getTorrents(url: str) -> dict:
 
         if len(rows) == 0:
             break
-        
+
+    print(count)
     return torrents
 
 def _choiceD(dict: dict, subElem = "") -> str:
@@ -94,6 +114,10 @@ def ask(prompt: str) -> str:
     
 
 if __name__ == '__main__':
+
+    if useTor:
+        os.system('sudo systemctl start tor')
+        time.sleep(0.1)
     query = " ".join(sys.argv[1:]).replace(" ", "+")
     if len(query) == 0:
         query = ask("Search tags: ").replace(" ", "+")
@@ -102,6 +126,9 @@ if __name__ == '__main__':
     logging.info(f"Got {str(len(torrents))} total entries")
     if len(torrents) == 0:
         sys.exit(1)
+
+    if useTor:
+        os.system('sudo systemctl stop tor')
     magnet = choice(torrents, subElem="name").get("magnet")
     logging.info(f"Got magnet link: {magnet}")
     
